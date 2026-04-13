bda60c071f993f88e71b6b0354c9310a9027bca3
Fer <edwardshanti3@gmail.com>
2026-04-12 18:25:10 -0600

    Eliminar _debugEvent.js

--- plugins/_debugEvent.js
+++
@@ -1,9 +0,0 @@
-export async function before(m, { conn }) {
-  if (m.isGroup && (m.messageStubType === 27 || m.messageStubType === 29)) {
-    const tipo = m.messageStubType === 27 ? '📥 ENTRADA' : '📤 SALIDA';
-    await conn.sendMessage(m.chat, {
-      text: `🔔 *EVENTO DETECTADO*\n\n> ${tipo}\n> Participantes: ${JSON.stringify(m.messageStubParameters)}\n> messageStubType: ${m.messageStubType}`
-    });
-  }
-  return true;
-}
