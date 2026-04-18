817bd5935ee877b777b78b6329510fd9d0675b7a
Fer <edwardshanti3@gmail.com>
2026-04-18 17:47:20 -0600

    Actualizar group.js

--- plugins/group.js
+++ plugins/group.js
@@ -97,7 +97,7 @@ let handler = async (m, { conn, text, usedPrefix, command, isAdmin, isROwner, is
 };
 
 handler.help = ['lockgroup <tiempo>'];
-handler.tags = ['admin'];
+handler.tags = ['group'];
 handler.command = /^(lockgroup|cerrar|abrirgrupo|grupo)$/i;
 handler.group = true;
 export default handler;
