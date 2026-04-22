5d8933722df871a0980ec693fbcc862dad42bd85
Fer <edwardshanti3@gmail.com>
2026-04-21 21:39:10 -0600

    Actualizar rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -5,22 +5,22 @@ let handler = async (m, { conn }) => {
   if (user.saes === undefined) user.saes = 100
   if (user.bank === undefined) user.bank = 0
 
-  m.reply(`
+  let total = user.saes + user.bank
+
+  let texto = `
 > *•───⧼⧼⧼ 𝘽𝘼𝙇𝘼𝙉𝘾𝙀 ⧽⧽⧽───•*
 
 > @${m.sender.split('@')[0]}
 
 > *• 𝐃𝐢𝐚𝐦𝐚𝐧𝐭𝐞:* _${user.diamond} 💎_
 > *• 𝐒𝐚𝐞𝐬:* _${user.saes} 🪙_
-> *Afuera del Banco*
-
-> *•───⧼⧼⧼ 𝘽𝘼𝙉𝘾𝙊 ⧽⧽⧽───•*
-
-> *🏦 𝐃𝐢𝐧𝐞𝐫𝐨 :* _${user.bank} 🪙_
-> *Adentro del Banco 🏦*
+> *• 𝐁𝐚𝐧𝐜𝐨:* _${user.bank} 🪙_
+> *• 𝐓𝐨𝐭𝐚𝐥:* _${total} 🪙_
 
 > *•───────────────•*
-`, { mentions: [m.sender] })
+`
+
+  conn.sendMessage(m.chat, { text: texto, mentions: [m.sender] })
 }
 
 handler.help = ["balance"]
