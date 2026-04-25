fdd69df15a4ea1713a1519e273e1aa0998071bcb
Fer <edwardshanti3@gmail.com>
2026-04-24 18:21:42 -0600

    Actualizar anime-sad.js

--- plugins/anime-sad.js
+++ plugins/anime-sad.js
@@ -1,31 +1,22 @@
-import fetch from 'node-fetch'
-
 let handler = async (m, { conn }) => {
   let gifs = [
-    "https://files.catbox.moe/5cm82n.gif",
-    "https://files.catbox.moe/t79ulr.gif",
-    "https://files.catbox.moe/mvpkmn.gif"
+    "https://media.tenor.com/-Kw6P1jJqcoAAAAC/anime-sad.gif",
+    "https://media.tenor.com/oB8UuNuYfZQAAAAC/sad-anime.gif",
+    "https://media.tenor.com/YpHw7QV6hbMAAAAC/anime-cry.gif"
   ]
   let randomGif = gifs[Math.floor(Math.random() * gifs.length)]
   
-  // Descargar el GIF
-  let res = await fetch(randomGif)
-  let buffer = await res.buffer()
-  
   let respuesta = `> *•───⧼⧼⧼ 𝙰𝙽𝙸𝙼𝙴 𝚂𝙰𝙳 ⧽⧽⧽───•*\n\n> *😔 @${m.sender.split('@')[0]} está triste...*\n\n> *"Hasta el aula de élite tiene momentos de oscuridad"*\n\n> *•───────────────•*`
   
-  // Enviar como GIF (la forma correcta en Baileys)
   await conn.sendMessage(m.chat, {
-    video: buffer,
+    video: { url: randomGif },
     gifPlayback: true,
     caption: respuesta,
-    mentions: [m.sender],
-    mimetype: 'video/mp4'
+    mentions: [m.sender]
   })
 }
 
 handler.help = ['sad']
 handler.tags = ['anime']
 handler.command = ['sad', 'triste', 'depre']
-
 export default handler
