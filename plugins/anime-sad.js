315c76f0a165de6edb7bf3282f8f6d7559c102dd
Fer <edwardshanti3@gmail.com>
2026-04-24 18:15:24 -0600

    Actualizar anime-sad.js

--- plugins/anime-sad.js
+++ plugins/anime-sad.js
@@ -1,9 +1,29 @@
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-  m.reply("sad funciona")
+  let gifs = [
+    "https://files.catbox.moe/5cm82n.gif",
+    "https://files.catbox.moe/t79ulr.gif",
+    "https://files.catbox.moe/mvpkmn.gif"
+  ]
+  let randomGif = gifs[Math.floor(Math.random() * gifs.length)]
+  
+  // Descargar el GIF
+  let res = await fetch(randomGif)
+  let buffer = await res.buffer()
+  
+  let respuesta = `> *•───⧼⧼⧼ 𝙰𝙽𝙸𝙼𝙴 𝚂𝙰𝙳 ⧽⧽⧽───•*\n\n> *😔 @${m.sender.split('@')[0]} está triste...*\n\n> *"Hasta el aula de élite tiene momentos de oscuridad"*\n\n> *•───────────────•*`
+  
+  await conn.sendMessage(m.chat, {
+    video: buffer,
+    gifPlayback: true,
+    caption: respuesta,
+    mentions: [m.sender]
+  })
 }
-handler.command = ["sad"]
-handler.help ["sad"]
-handler.tags ["anime"]
+
+handler.help = ['sad']
+handler.tags = ['anime']
+handler.command = ['sad', 'triste', 'depre']
 
 export default handler
