b1ea12d2db4e946f4f40e4000f8226ad0f57727d
Fer <edwardshanti3@gmail.com>
2026-04-24 18:03:12 -0600

    Crear anime-sad.js

---
+++ plugins/anime-sad.js
@@ -0,0 +1,34 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let user = m.sender
+  let name = await conn.getName(user)
+  
+  let gifs = [
+    "https://files.catbox.moe/5cm82n.gif",
+    "https://files.catbox.moe/t79ulr.gif",
+    "https://files.catbox.moe/mvpkmn.gif"
+  ]
+  
+  let randomGif = gifs[Math.floor(Math.random() * gifs.length)]
+  
+  let respuesta = `
+> *•───⧼⧼⧼ 𝙰𝙽𝙸𝙼𝙴 𝚂𝙰𝙳 ⧽⧽⧽───•*
+
+> *😔 @${m.sender.split('@')[0]} está triste...*
+
+> *"Hasta el aula de élite tiene momentos de oscuridad"*
+
+> *•───────────────•*
+`
+
+  await conn.sendMessage(m.chat, {
+    gif: { url: randomGif },
+    caption: respuesta,
+    mentions: [user]
+  })
+}
+
+handler.help = ["sad"]
+handler.tags = ["anime"]
+handler.command = ["sad", "triste", "depre"]
+
+export default handler
