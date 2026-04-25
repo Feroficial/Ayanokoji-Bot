b2d6949277f3591dd39d8d8327ddf11e71edd1c5
Fer <edwardshanti3@gmail.com>
2026-04-24 18:29:10 -0600

    Actualizar anime-sad.js

--- plugins/anime-sad.js
+++ plugins/anime-sad.js
@@ -1,18 +1,15 @@
-import fs from 'fs'
-
 let handler = async (m, { conn }) => {
   let gifs = [
-    "./tmp/sad1.gif",
-    "./tmp/sad2.gif",
-    "./tmp/sad3.gif"
+    "https://i.imgur.com/KcYvRw7.gif",
+    "https://i.imgur.com/3X4YwzQ.gif",
+    "https://i.imgur.com/7YKpG4v.gif"
   ]
   let randomGif = gifs[Math.floor(Math.random() * gifs.length)]
-  let buffer = fs.readFileSync(randomGif)
   
   let respuesta = `> *•───⧼⧼⧼ 𝙰𝙽𝙸𝙼𝙴 𝚂𝙰𝙳 ⧽⧽⧽───•*\n\n> *😔 @${m.sender.split('@')[0]} está triste...*\n\n> *"Hasta el aula de élite tiene momentos de oscuridad"*\n\n> *•───────────────•*`
   
   await conn.sendMessage(m.chat, {
-    video: buffer,
+    video: { url: randomGif },
     gifPlayback: true,
     caption: respuesta,
     mentions: [m.sender]
