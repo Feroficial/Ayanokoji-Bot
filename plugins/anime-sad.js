567de108024cd809e296343c346a02fab79f2fb7
Fer <edwardshanti3@gmail.com>
2026-04-24 18:27:07 -0600

    Actualizar anime-sad.js

--- plugins/anime-sad.js
+++ plugins/anime-sad.js
@@ -1,15 +1,18 @@
+import fs from 'fs'
+
 let handler = async (m, { conn }) => {
   let gifs = [
-    "https://media.tenor.com/-Kw6P1jJqcoAAAAC/anime-sad.gif",
-    "https://media.tenor.com/oB8UuNuYfZQAAAAC/sad-anime.gif",
-    "https://media.tenor.com/YpHw7QV6hbMAAAAC/anime-cry.gif"
+    "./tmp/sad1.gif",
+    "./tmp/sad2.gif",
+    "./tmp/sad3.gif"
   ]
   let randomGif = gifs[Math.floor(Math.random() * gifs.length)]
+  let buffer = fs.readFileSync(randomGif)
   
   let respuesta = `> *•───⧼⧼⧼ 𝙰𝙽𝙸𝙼𝙴 𝚂𝙰𝙳 ⧽⧽⧽───•*\n\n> *😔 @${m.sender.split('@')[0]} está triste...*\n\n> *"Hasta el aula de élite tiene momentos de oscuridad"*\n\n> *•───────────────•*`
   
   await conn.sendMessage(m.chat, {
-    video: { url: randomGif },
+    video: buffer,
     gifPlayback: true,
     caption: respuesta,
     mentions: [m.sender]
@@ -19,4 +22,5 @@ let handler = async (m, { conn }) => {
 handler.help = ['sad']
 handler.tags = ['anime']
 handler.command = ['sad', 'triste', 'depre']
+
 export default handler
