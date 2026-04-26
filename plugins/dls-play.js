e4cc0769d6b8b22db69872daabd760661b2ede14
Fer <edwardshanti3@gmail.com>
2026-04-25 21:04:46 -0600

    Actualizar dls-play.js

--- plugins/dls-play.js
+++ plugins/dls-play.js
@@ -158,5 +158,6 @@ const extractVideoId = (url) => {
 
 handler.help = ["play"]
 handler.tags = ["downloader"]
-handler.command = ["play", "yt", "yta", "audio"]
+handler.command = ["play", "yt", "yta", "audio"
+handler.register = true
 export default handler
