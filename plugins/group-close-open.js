6e8f25284bcebb7aa7643f53359a19c8daae02ed
Fer <edwardshanti3@gmail.com>
2026-04-30 22:12:07 -0600

    Actualizar group-close-open.js

--- plugins/group-close-open.js
+++ plugins/group-close-open.js
@@ -1,38 +1,26 @@
-let handler = async (m, { conn, text }) => {
-    if (!m.isGroup) return m.reply('🌸 Este comando solo funciona en grupos')
-    
-    const groupId = m.chat
-    const groupMetadata = await conn.groupMetadata(groupId)
-    const botNumber = conn.user.jid.split('@')[0]
-    
-    let botIsAdmin = false
-    for (let p of groupMetadata.participants) {
-        const participantNumber = p.id.split('@')[0]
-        if (participantNumber === botNumber && (p.admin === 'admin' || p.admin === 'superadmin')) {
-            botIsAdmin = true
-            break
-        }
-    }
-    
-    if (!botIsAdmin) return m.reply('🌸 La bot necesita ser administradora')
+let handler = async (m, { conn, text, isAdmin, isOwner, isROwner }) => {
+    if (!m.isGroup) return m.reply('🌸 Este comando solo funciona en grupos');
+    if (!isAdmin && !isOwner && !isROwner) return m.reply('🌸 Solo administradoras o la creadora');
+
+    const groupId = m.chat;
     
     if (text === 'open') {
-        await conn.groupSettingUpdate(groupId, 'not_announcement')
+        await conn.groupSettingUpdate(groupId, 'not_announcement');
         m.reply(`🌸 *— ✧ 𝐆𝐑𝐔𝐏𝐎 𝐀𝐁𝐈𝐄𝐑𝐓𝐎 ✧ —* 🌸
         
 > 💗 *El grupo ha sido abierto*
 > 🎀 *Todas las participantes pueden enviar mensajes*
 
-🌸 *Danny Yulieth* 🌸`)
+🌸 *Danny Yulieth* 🌸`);
     } 
     else if (text === 'close') {
-        await conn.groupSettingUpdate(groupId, 'announcement')
+        await conn.groupSettingUpdate(groupId, 'announcement');
         m.reply(`🌸 *— ✧ 𝐆𝐑𝐔𝐏𝐎 𝐂𝐄𝐑𝐑𝐀𝐃𝐎 ✧ —* 🌸
         
 > 💗 *El grupo ha sido cerrado*
 > 🎀 *Solo administradoras pueden enviar mensajes*
 
-🌸 *Danny Yulieth* 🌸`)
+🌸 *Danny Yulieth* 🌸`);
     } 
     else {
         m.reply(`🌸 *— ✧ 𝐂𝐎𝐌𝐀𝐍𝐃𝐎 𝐆𝐑𝐔𝐏𝐎 ✧ —* 🌸
@@ -42,14 +30,14 @@ let handler = async (m, { conn, text }) => {
 
 > ✨ *Ejemplo:* #grupo open
 
-🌸 *Danny Yulieth* 🌸`)
+🌸 *Danny Yulieth* 🌸`);
     }
 }
 
-handler.help = ['grupo <open/close>']
-handler.tags = ['group']
-handler.command = ['grupo', 'grupos']
-handler.group = true
-handler.admin = true
+handler.help = ['grupo <open/close>'];
+handler.tags = ['group'];
+handler.command = ['grupo', 'grupos'];
+handler.group = true;
+handler.admin = true;
 
-export default handler
+export default handler;
