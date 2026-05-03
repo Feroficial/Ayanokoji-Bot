547a463a721037c4a64e39cbf6d82bca9ac6bcd7
Fer <edwardshanti3@gmail.com>
2026-05-02 23:11:39 -0600

    Actualizar group-abrir-cerrar.js

--- plugins/group-abrir-cerrar.js
+++ plugins/group-abrir-cerrar.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, isAdmin, isOwner, args, text }) => {
+let handler = async (m, { conn, isAdmin, isOwner, args }) => {
   if (!m.isGroup) return m.reply(`
 ㅤ    ꒰  ㅤ ❌ ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ єяяσя 木 ɢяυρσ ㅤ 性
@@ -17,10 +17,10 @@ let handler = async (m, { conn, isAdmin, isOwner, args, text }) => {
 ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
   `.trim())
 
-  const input = text?.toLowerCase() || args[0]?.toLowerCase() || ''
+  const input = args[0]?.toLowerCase() || ''
   
   if (input === 'abrir' || input === 'open') {
-    await conn.groupSettingUpdate(m.chat, 'unlocked')
+    await conn.groupSettingUpdate(m.chat, 'not_announcement')
     await m.reply(`
 ㅤ    ꒰  ㅤ 🔓 ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ αвяιя 木 gяυρσ ㅤ 性
@@ -31,7 +31,7 @@ let handler = async (m, { conn, isAdmin, isOwner, args, text }) => {
 ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
     `.trim())
   } else if (input === 'cerrar' || input === 'close') {
-    await conn.groupSettingUpdate(m.chat, 'locked')
+    await conn.groupSettingUpdate(m.chat, 'announcement')
     await m.reply(`
 ㅤ    ꒰  ㅤ 🔒 ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ ¢єяяαя 木 gяυρσ ㅤ 性
@@ -46,7 +46,7 @@ let handler = async (m, { conn, isAdmin, isOwner, args, text }) => {
 ㅤ    ꒰  ㅤ 📝 ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ υѕσ 木 cσrrєctσ ㅤ 性
 
-> ₊· ⫏⫏ ㅤ *Uѕσ:* #αвяιя  σ  #¢єяяαя
+> ₊· ⫏⫏ ㅤ *Uѕσ:* #abrir  o  #cerrar
 
 ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
     `.trim())
