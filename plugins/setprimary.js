23147c7e443433ad04b0b54251fa5802705c6a98
Fer <edwardshanti3@gmail.com>
2026-05-03 21:04:38 -0600

    Actualizar setprimary.js

--- plugins/setprimary.js
+++ plugins/setprimary.js
@@ -18,45 +18,50 @@ let handler = async (m, { conn, isOwner, args }) => {
 ㅤ    ⿻ ㅤ ✿ ㅤ υѕσ 木 cσrrєctσ ㅤ 性
 
 > ₊· ⫏⫏ ㅤ *Uѕσ:* #ѕєтρяιмαяу @вσт ση/σƒƒ
+> ₊· ⫏⫏ ㅤ *Ejeмρℓσ:* #ѕєтρяιмαяу @${conn.user.jid.split('@')[0]} ση
 
 ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
 > ₊· ⫏⫏ ㅤ 🔖 Cяєα∂σя: Lʏᴏɴɴ
   `.trim())
   
+  if (!global.db.data.settings[mention]) {
+    global.db.data.settings[mention] = {}
+  }
+  
   if (opcion === 'on') {
-    if (!global.db.data.settings[mention]) global.db.data.settings[mention] = {}
     global.db.data.settings[mention].primary = true
     await m.reply(`
 ㅤ    ꒰  ㅤ 👑 ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ вσт 木 ρяιмαяισ ㅤ 性
 
-> ₊· ⫏⫏ ㅤ *🤖 Bot mencionado:* @${mention.split('@')[0]}
+> ₊· ⫏⫏ ㅤ *🤖 Bot mencionado:* ${mention.split('@')[0]}
 > ₊· ⫏⫏ ㅤ *✅ Estado:* PRINCIPAL activado
 
 ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
 > ₊· ⫏⫏ ㅤ 🔖 Cяєα∂σя: Lʏᴏɴɴ
-    `.trim(), { mentions: [mention] })
+    `.trim())
   } 
   else if (opcion === 'off') {
-    if (!global.db.data.settings[mention]) global.db.data.settings[mention] = {}
     global.db.data.settings[mention].primary = false
     await m.reply(`
 ㅤ    ꒰  ㅤ 🔒 ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
 ㅤ    ⿻ ㅤ ✿ ㅤ вσт 木 ѕє¢υη∂αяισ ㅤ 性
 
-> ₊· ⫏⫏ ㅤ *🤖 Bot mencionado:* @${mention.split('@')[0]}
+> ₊· ⫏⫏ ㅤ *🤖 Bot mencionado:* ${mention.split('@')[0]}
 > ₊· ⫏⫏ ㅤ *❌ Estado:* PRINCIPAL desactivado
 
 ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
 > ₊· ⫏⫏ ㅤ 🔖 Cяєα∂σя: Lʏᴏɴɴ
-    `.trim(), { mentions: [mention] })
+    `.trim())
   }
   else {
+    let estado = global.db.data.settings[mention].primary ? '✅ ACTIVADO' : '❌ DESACTIVADO'
     await m.reply(`
-ㅤ    ꒰  ㅤ 📝 ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
-ㅤ    ⿻ ㅤ ✿ ㅤ υѕσ 木 cσrrєctσ ㅤ 性
+ㅤ    ꒰  ㅤ 📊 ㅤ *αℓуα - вσт* ㅤ ⫏⫏  ꒱
+ㅤ    ⿻ ㅤ ✿ ㅤ єѕтα∂σ 木 вσт ㅤ 性
 
-> ₊· ⫏⫏ ㅤ *Uѕσ:* #ѕєтρяιмαяу @вσт ση/σƒƒ
+> ₊· ⫏⫏ ㅤ *🤖 Bot:* ${mention.split('@')[0]}
+> ₊· ⫏⫏ ㅤ *📌 Estado:* ${estado}
 
 ㅤ    ꒰  ㅤ ✿ ㅤ *αℓуα - вσт* ㅤ ⫏⫏ ꒱
 > ₊· ⫏⫏ ㅤ 🔖 Cяєα∂σя: Lʏᴏɴɴ
